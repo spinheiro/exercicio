1b41dd36649745b790aa3ae501abdbae4770ac5a
Samuel Pinheiro <samuel.flf@gmail.com>
2019-12-16 08:27:07 -0300

    Estado de classe

--- src/Menu.js
+++ src/Menu.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { createDrawerNavigator } from 'react-navigation'
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Simples from './componentes/Simples'
+import Contador from  './componentes/Contador'
 
 
 export default createDrawerNavigator({
-
+    Contador: {
+        screen: () => <Contador numeroInicial={100}/>
+    },
     Texto: {
         screen: () =>  <Text> Bllaaaa </Text>
 
